cdabcac88494b827f55f28ecc8f6568830409f6a
Peter J. Truong <hp5340@gmail.com>
2017-02-24 20:53:27 -0800

    initial commit

---
+++ src/main/java/com/teamtreehouse/giflib/dao/CategoryDaoImpl.java
@@ -0,0 +1,68 @@
+package com.teamtreehouse.giflib.dao;
+
+import com.teamtreehouse.giflib.model.Category;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CategoryDaoImpl implements CategoryDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Category> findAll() {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Get all categories with a Hibernate criteria
+        List<Category> categories = session.createCriteria(Category.class).list();
+
+        // Close session
+        session.close();
+
+        return categories;
+    }
+
+    @Override
+    public Category findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Category category = session.get(Category.class, id);
+        Hibernate.initialize(category.getGifs());
+        session.close();
+        return category;
+    }
+
+    @Override
+    public void save(Category category) {
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Begin a transaction
+        session.beginTransaction();
+
+        // Save the category
+        session.saveOrUpdate(category);
+
+        // Commit the transaction
+        session.getTransaction().commit();
+
+        // Close the session
+        session.close();
+    }
+
+    @Override
+    public void delete(Category category) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.delete(category);
+        session.getTransaction().commit();
+        session.close();
+
+    }
+}
---
+++ src/main/java/com/teamtreehouse/giflib/dao/GifDao.java
@@ -0,0 +1,14 @@
+package com.teamtreehouse.giflib.dao;
+
+import com.teamtreehouse.giflib.model.Gif;
+
+import java.util.List;
+
+public interface GifDao {
+    List<Gif> findAll();
+    Gif findById(Long id);
+    void save(Gif gif);
+    void delete(Gif gif);
+    List<Gif> findFavorite();
+    List<Gif> findSearch(String q);
+}
